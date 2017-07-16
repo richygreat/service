4ab1ad57236a4a049821b43ee8172b3cd6ef37d0
Richy <richygreat@gmail.com>
2017-07-16 08:19:22 +0530

    Money functions for Loans

--- src/main/java/com/rg/service/entity/Money.java
+++ src/main/java/com/rg/service/entity/Money.java
@@ -24,21 +24,28 @@ public class Money implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long moneyId;
 	private Double amount;
+	private Double interestRate;
+	private Double emi;
+	private int instMonths;
+	private int remainInstMonths;
 	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
 	private Type type;
 	private Date date;
+	private Date amortStartDate;
 	private String description;
 	private Boolean credit;
-	@ManyToOne(fetch=FetchType.EAGER)
-    @JoinColumn(name = "personId")
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "personId")
 	private User user;
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public String toString() {
-		return "Money [moneyId=" + moneyId + ", amount=" + amount + ", type=" + type + ", date=" + date
-				+ ", description=" + description + ", credit=" + credit + "]";
+		return "Money [moneyId=" + moneyId + ", amount=" + amount + ", interestRate=" + interestRate + ", emi=" + emi
+				+ ", instMonths=" + instMonths + ", remainInstMonths=" + remainInstMonths + ", type=" + type + ", date="
+				+ date + ", amortStartDate=" + amortStartDate + ", description=" + description + ", credit=" + credit
+				+ ", user=" + user + "]";
 	}
 
 	public Long getMoneyId() {
@@ -95,5 +102,59 @@ public class Money implements Serializable {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public Date getAmortStartDate() {
+		return amortStartDate;
+	}
+
+	public void setAmortStartDate(Date amortStartDate) {
+		this.amortStartDate = amortStartDate;
+	}
+
+	public Double getEmi() {
+		if (emi == null) {
+			emi = getEmiAmount(this);
+		}
+		return emi;
+	}
+
+	public void setEmi(Double emi) {
+		this.emi = emi;
+	}
+
+	public int getInstMonths() {
+		return instMonths;
+	}
+
+	public void setInstMonths(int instMonths) {
+		this.instMonths = instMonths;
+	}
+
+	public int getRemainInstMonths() {
+		return remainInstMonths;
+	}
+
+	public void setRemainInstMonths(int remainInstMonths) {
+		this.remainInstMonths = remainInstMonths;
+	}
+
+	public static double getEmiAmount(Money money) {
+		return Math.round(money.getAmount() * (money.getInterestRate() / 1200)
+				/ (1 - (1 / Math.pow(1 + (money.getInterestRate() / 1200), money.getInstMonths()))));
+	}
+
+	public static double getBalanceAmount(Money money) {
+		return Math.round(
+				money.getEmi() * (1 - (1 / Math.pow(1 + (money.getInterestRate() / 1200), money.getRemainInstMonths())))
+						/ (money.getInterestRate() / 1200));
 	}
 }
